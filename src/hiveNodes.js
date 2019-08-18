76da2a3fbfc7a549f463e9ecf27fc55b877e8cba
Daniel Zigerlig <daniel.zigerlig@swisscom.com>
2019-08-18 23:27:19 +0200

    add create hiveNode form

--- src/hiveNodes.js
+++ src/hiveNodes.js
@@ -21,6 +21,15 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import * as firebase from 'firebase';
 
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FilledInput from '@material-ui/core/FilledInput';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Input from '@material-ui/core/Input';
+
 var provider = new firebase.auth.GoogleAuthProvider();
 
 function googleLogin (){
@@ -53,7 +62,13 @@ class HiveNodes extends Component {
             hiveOrigin: hiveOrigin,
             hives: [ ],
             hiveNodes: [],
-            value: null
+            value: null,
+            hiveNodeName: '',
+            hiveNodeDescription: '',
+            hiveNodeUrl: '',
+            hiveNodeImage: '',
+            hiveNodeOrigin: hiveOrigin,
+            hiveNodePreviousNode: ''
         };
 
     }
@@ -115,6 +130,37 @@ class HiveNodes extends Component {
         })
     };
 
+    handleSubmit(event, dev) {
+        event.preventDefault();
+        alert('Adding new HiveNode (name: ' + dev.state.hiveNodeName + ', description: ' + dev.state.hiveNodeDescription + ', nodeUrl: ' + dev.state.hiveNodeUrl + ', nodeImageUrl' + dev.state.hiveNodeImage +  ', hiveNodeOrigin: ' + dev.state.hiveNodeOrigin + ', hiveNodePreviousNode: ' + dev.state.hiveNodePreviousNode + ')');
+
+        const Run = window.Run;
+        const run = new Run();
+
+//        const hive = new Hive(
+//                dev.state.name,
+//                dev.state.description,
+//                run.owner.pubkey.toString(),
+//                dev.state.category,
+//                parseInt(dev.state.satoshis),
+//                dev.state.image);
+
+        const hiveNode = new HiveNode(
+                    dev.state.hiveNodeName,
+                    dev.state.hiveNodeDescription,
+                    run.owner.pubkey.toString(),
+                    dev.state.hiveNodeUrl,
+                    dev.state.hiveNodeImage,
+                    dev.state.hiveNodeOrigin,
+                    dev.state.hiveNodePreviousNode);
+
+         hiveNode.sync().then(createdHiveNode => {
+            alert('HiveNode created, hooray, please wait a bit until you see it on the main page');
+         }).catch(function(rejected) {
+            alert('Error while creating HiveNode: ' + rejected);
+         });
+      }
+
     render() {
 
         const {value, hiveNodes} = this.state;
@@ -173,16 +219,62 @@ class HiveNodes extends Component {
                                           </CardContent>
                                       </CardActionArea>
                                       <CardActions>
-                                          <Button size="small" color="primary">
-                                              More
+                                          <Button size="small" color="primary" onClick={(e) => this.setState({hiveNodePreviousNode : hive.origin})}>
+                                              Add link
                                           </Button>
-
                                       </CardActions>
                                   </Card>
                              </Grid>
                         )}
 
                       </Grid>
+                      <form ref="form" onSubmit={(e) => this.handleSubmit(e, this)}>
+                              <div style={{marginTop: 60, marginLeft: 240}}>
+                              <div style={{paddingLeft: 150, paddingTop: 50}}>
+                                   <Input
+                                      placeholder="Name"
+                                      value={this.state.hiveNodeName}
+                                      onChange={(e) => this.setState({hiveNodeName : e.target.value})}
+                                   />
+                                   <br/>
+                                    <Input
+                                      placeholder="Description"
+                                       value={this.state.hiveNodeDescription}
+                                      onChange={(e) => this.setState({hiveNodeDescription : e.target.value})}
+                                   />
+                                   <br/>
+                                    <Input
+                                      placeholder="Image URL"
+                                       value={this.state.hiveNodeImage}
+                                      onChange={(e) => this.setState({hiveNodeImage : e.target.value})}
+                                   />
+                                   <br/>
+                                <br />
+                                    <Input
+                                      placeholder="URL"
+                                       value={this.state.hiveNodeUrl}
+                                      onChange={(e) => this.setState({hiveNodeUrl : e.target.value})}
+                                   />
+                                   <br/>
+                                    <Input
+                                      placeholder="Linked Hive Node (Previous Node)"
+                                       value={this.state.hiveNodePreviousNode}
+                                      onChange={(e) => this.setState({hiveNodePreviousNode : e.target.value})}
+                                   />
+                                   <br/>
+                                    <Input
+                                      placeholder="Hive"
+                                      enabled="false"
+                                       value={this.state.hiveNodeOrigin}
+                                   />
+                                   <br/>
+
+                               <Button variant="contained" color="default" type="submit">
+                                  Submit
+                                </Button>
+                              </div>
+                              </div>
+                          </form>
                   </main>
               </div>
 
